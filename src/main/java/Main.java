fa805b85b2df54059d1ed35c833057966eed0003
RZHuangJeff <qwerty6933898@gmail.com>
2020-05-26 13:18:32 +0800

    Feat: Make program executable.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
-public class Main{
-    public static void main(String[] args){
+import control.*;
+import display.*;
+import layout.*;
 
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Window window = new Window("Minecraft", 600, 480, true);
+            GameLoopingController controller = new GameLoopingController(window);
+            Frame startFrame = new StartFrame(window, controller);
+            controller.addFrame(startFrame);
+            controller.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
     }
 }
